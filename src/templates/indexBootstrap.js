26b0dd16360f0d9bbdde14f80852dd4d5897b02a
TomNewChao <tom_toworld@163.com>
2026-01-15 14:25:58 +0800

    first commit

---
+++ src/templates/indexBootstrap.js
@@ -0,0 +1,7 @@
+
+(async () => {
+  window.$ = window.jQuery = require('ep_etherpad-lite/static/js/rjquery').jQuery;
+  require('ep_etherpad-lite/static/js/l10n')
+  require('ep_etherpad-lite/static/js/index')
+  require('ep_etherpad-lite/static/js/welcome')
+})()
